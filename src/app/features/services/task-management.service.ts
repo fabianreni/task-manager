c53cc9b6cefa04a7756deea1e1fb439a6e48799b
r.fabian <r.fabian@resource-it.ro>
2024-02-21 08:02:49 +0200

    add initaial tasks

--- src/app/features/services/task-management.service.ts
+++ src/app/features/services/task-management.service.ts
@@ -16,13 +16,13 @@ export class TaskManagementService {
   constructor(
     private taskDashboardService: TaskDashboardService) {
     this.tasks$ = this.tasksSubject.asObservable();
+    this.addInitialTasks();
   }
 
   createTask(task: Task): void {
     task.id = this.tasks.length + 1;
     this.tasks.push(task);
     this.tasksSubject.next(this.tasks);
-    this.taskDashboardService.setTaskSummary(this.tasks);
     this.setTaskSummary();
   }
 
@@ -55,7 +55,25 @@ export class TaskManagementService {
     this.setTaskSummary();
   }
 
+  private addInitialTasks(): void {
+    const task1 = new Task();
+    task1.id = 1;
+    task1.title = "Create project";
+    task1.description = "Create an angular project for task";
+    task1.status = 3;
+
+
+    const task2 = new Task();
+    task1.id = 2;
+    task2.title = "Add routing to project"
+    task2.description = "Create routing module and add somerouting";
+
+    this.tasks.push(task1, task2);
+    this.tasksSubject.next(this.tasks);
+  }
+
   private setTaskSummary(): void {
     this.taskDashboardService.setTaskSummary(this.tasks);
   }
+
 }
